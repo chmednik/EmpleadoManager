bb390e714cc96fb6a1072b10c2b0895fe7a09944
ITMaster <ITMaster@Mac-mini-de-ITMaster-5.local>
2016-07-07 21:29:42 -0300

    Mi primer commit

---
+++ src/main/java/com/mycompany/empleadomanager/modelo/Empleado.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.empleadomanager.modelo;
+
+public class Empleado {
+    private String nombre;
+    private Integer edad;
+    private double sueldo;
+    private Long id;
+    
+}
